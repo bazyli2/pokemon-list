ef6c8ce7fc0ab1e7057a56ed8f916fa025d8c85f
Jeremi Wielewski <wielewski.jeremi@gmail.com>
2025-11-24 12:44:46 +0100

    added styling to chips

--- src/components/PokemonTypes.tsx
+++ src/components/PokemonTypes.tsx
@@ -8,10 +8,11 @@ export function PokemonTypes() {
   if (isPending) return <div>loading...</div>;
   if (isError) return <div>{error.message}</div>;
   return (
-    <ul>
+    <ul className="flex gap-3">
       {data.map((pokemonType) => (
         <li key={pokemonType.name}>
           <Chip
+            className="font-semibold capitalize text-white"
             label={pokemonType.name}
             style={{ backgroundColor: pokemonType.color }}
           />

07312df126ccf3f57d4fe5298f52e924e85364ba
Jeremi Wielewski <wielewski.jeremi@gmail.com>
2025-11-24 14:27:37 +0100

    added styling to button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { PokemonTypes } from "@/components/PokemonTypes";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col px-5 items-center gap-6 pt-10">
       bla
       <PokemonTypes />
       <PokemonList />
--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -3,6 +3,7 @@
 import { usePokemonList } from "@/queries/usePokemonList";
 import { Fragment } from "react/jsx-runtime";
 import { PokemonCard } from "./PokemonCard";
+import { Button } from "@mui/material";
 
 export function PokemonList() {
   const { isPending, isError, data, error, fetchNextPage, hasNextPage } =
@@ -11,7 +12,7 @@ export function PokemonList() {
   if (isError) return <div>{error.message}</div>;
   return (
     <>
-      <ul className="grid grid-cols-3 gap-3">
+      <ul className="grid grid-cols-3 gap-3 self-stretch">
         {data.pages.map((group, i) => (
           <Fragment key={i}>
             {group.pokemons.map((pokemon) => (
@@ -20,7 +21,15 @@ export function PokemonList() {
           </Fragment>
         ))}
       </ul>
-      {hasNextPage && <button onClick={() => fetchNextPage()}>next</button>}
+      {hasNextPage && (
+        <Button
+          className="rounded-full font-semibold normal-case"
+          variant="contained"
+          onClick={() => fetchNextPage()}
+        >
+          Load More
+        </Button>
+      )}
     </>
   );
 }

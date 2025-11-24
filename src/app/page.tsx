3e92c7dbfde79cb2bf1613081c2b37deebc21954
Jeremi Wielewski <wielewski.jeremi@gmail.com>
2025-11-24 14:33:42 +0100

    added header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
 import { PokemonList } from "@/components/PokemonList";
 import { PokemonTypes } from "@/components/PokemonTypes";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col px-5 items-center gap-6 pt-10">
-      bla
+    <main className="flex flex-col px-5 items-center gap-6 pt-10">
+      <header className="flex items-center gap-3">
+        <Image
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png"
+          alt="Pokeball"
+          width={50}
+          height={50}
+        />
+        <h1 className="text-5xl text-orange-500 font-semibold">
+          Pokémon Explorer
+        </h1>
+      </header>
       <PokemonTypes />
       <PokemonList />
-    </div>
+    </main>
   );
 }

221af0725fc2ed9cfb231e43ee821b4fc5427dde
Jeremi Wielewski <wielewski.jeremi@gmail.com>
2025-11-24 11:56:58 +0100

    added tanstack provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Providers from "@/Providers";
 
 const poppins = Geist({
   variable: "--font-geist-sans",
@@ -20,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <body className={`${poppins.variable} antialiased min-h-full`}>
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
